8b34cd29005ead9d7933d6d3f81d594fa1c0b923
MalyshAnanas <116721053+MalyshAnanas@users.noreply.github.com>
2024-03-29 00:30:23 +0200

    Add Lab_3 (index.php)

--- Lab 3/code/index.php
+++ Lab 3/code/index.php
@@ -26,16 +26,23 @@ echo "Найдено подстрок: " . $cnt . "<br>";
 var_dump($Dudu);
 
 // Пункт 1.b
-echo "<br>" . "~~~~Пункт №1.b~~~~" . "<br>";
+echo "<br><br>" . "~~~~Пункт №1.b~~~~" . "<br>";
 $regExp = '/[0-9]/';
 $str = 'a1b2c3';
 $Kuromi = array();
-$result = preg_replace_callback($regExp, function ($Kuromi) {
-    return pow(intval($Kuromi[0]), 3);
-},
-    $str);
-echo "Новая строчка: ". $result;
+$result = preg_replace_callback($regExp, function ($Kuromi) {return pow(intval($Kuromi[0]), 3);}, $str);
+echo "Новая строчка: ". $result."<br>";
+
+// Пункт 2.a
+echo "<br>" . "~~~~Пункт №2.a~~~~" . "<br>";
 ?>
+<a href="2_punkt/2_a.php">Второе задание. Пункт а</a>
+<p> ~~~~Пункт №2.b~~~~ <br> </p>
+<a href="2_punkt/2_b.php">Второе задание. Пункт b</a>
+<p> ~~~~Пункт №2.c~~~~ <br> </p>
+<a href="2_punkt/2_c.php">Второе задание. Пункт c</a>
+<p>  ~~~~Пункт №3~~~~ <br> </p>
+<a href="3_punkt/3.php">Третье задание</a>
 </body>
 </html>
 
